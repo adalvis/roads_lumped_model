750dac96329f9a741715f29427b1bd4692ddaa89
Amanda Manaster <amanaste@uw.edu>
2017-03-30 16:46:03 -0700

    Updated code
    Made all three plots appear; ran model for 24 hrs and for 2 hrs to get
    the hydrograph and the water depth, respectively.

--- OverlandFlow/OverlandFlow_test.py
+++ OverlandFlow/OverlandFlow_test.py
@@ -1,6 +1,8 @@
-#Author: Amanda Manaster
-#Date: 03/30/2017
-#Purpose: Playing with the Landlab component OverlandFlow.
+"""
+Author: Amanda Manaster
+Date: 03/30/2017
+Purpose: Playing with the Landlab component OverlandFlow.
+"""
 
 #import usual Python packages
 import numpy as np
@@ -22,16 +24,18 @@ watershed_dem = 'Square_TestBasin.asc'
 rmg.set_watershed_boundary_condition(z, nodata_value=-9999)
 
 #fill sinks to ensure complete drainage
-sf = SinkFiller(rmg, routing = 'D8', apply_slope = True, fill_slope = 1.e-5)
+sf = SinkFiller(rmg, routing = 'D4', apply_slope = True, fill_slope = 1.e-5)
 sf.fill_pits()
 
 #look at the pretty grid
-imshow_grid(rmg, 'topographic__elevation', plot_name = 'Topographic Elevation', var_name = 'Elevation', var_units = 'm',
-            grid_units = ('m','m'), cmap = 'pink')
+imshow_grid(rmg, 'topographic__elevation', plot_name = 'Topographic Elevation', 
+            var_name = 'Elevation', var_units = 'm',grid_units = ('m','m'), 
+            cmap = 'pink')
 plt.show()
 
+#initialize values for q calculations
 elapsed_time = 0.0
-model_run_time = 7200.
+model_run_time = 86400.
 of = OverlandFlow(rmg, steep_slopes = True)
 
 storm_duration = 7200.0
@@ -49,20 +53,40 @@ while elapsed_time < model_run_time:
         of.rainfall_intensity = 0.0
     of.overland_flow()
     
-    rmg.at_node['surface_water__discharge'] = (map_max_of_inlinks_to_node(rmg, np.abs(of.q)*rmg.dx))
+    rmg.at_node['surface_water__discharge'] = (map_max_of_inlinks_to_node(rmg,
+               np.abs(of.q)*rmg.dx))
 
     hydrograph_time.append(elapsed_time/3600.)
     discharge_at_outlet.append(np.abs(of.q[outlet_link])*rmg.dx)
                                            
     elapsed_time += of.dt
-    
-imshow_grid(rmg, 'surface_water__depth', plot_name = 'Water depth at time = 2 hr', var_name = 'Water depth', 
-            var_units = 'm', grid_units = ('m','m'), cmap = 'Blues')
-plt.show()
 
 plt.plot(hydrograph_time, discharge_at_outlet)
 axes = plt.gca()
 plt.xlabel('Time (hr)')
 plt.ylabel('Discharge (cms)')
 plt.title('Outlet Hydrograph, Rainfall: 5mm/hr in 2 hr')
+plt.show()
+
+#re-initialize to get water depth map at t = 2hr
+elapsed_time = 0.0
+hydrograph_time = []
+discharge_at_outlet = []
+
+while elapsed_time < storm_duration:
+    of.dt = of.calc_time_step()
+    of.rainfall_intensity = rainfall_mmhr * (2.777778*10**-7)
+    of.overland_flow()
+    
+    rmg.at_node['surface_water__discharge'] = (map_max_of_inlinks_to_node(rmg, 
+               np.abs(of.q)*rmg.dx))
+
+    hydrograph_time.append(elapsed_time/3600.)
+    discharge_at_outlet.append(np.abs(of.q[outlet_link])*rmg.dx)
+                                           
+    elapsed_time += of.dt
+
+imshow_grid(rmg, 'surface_water__depth', plot_name 
+            = 'Water depth at time = 2 hr', var_name = 'Water depth', 
+            var_units = 'm', grid_units = ('m','m'), cmap = 'Blues')
 plt.show()
