fb456cbb52655ac4dc9e868e5c1d0ee1def4246e
Amanda Manaster <amanaste@uw.edu>
2018-03-08 18:27:27 -0800

    Create road surface
    Code updated such that a road surface was actually created.

--- OverlandFlow/Road_Surface.py
+++ OverlandFlow/Road_Surface.py
@@ -5,19 +5,68 @@ Author: Amanda Manaster
 """
 
 from landlab import RasterModelGrid
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 from landlab.plot.imshow import imshow_grid
 
-grid = RasterModelGrid(355,47,0.225)
-elev = np.zeros(grid.number_of_nodes, dtype = float)
+mpl.rcParams['font.sans-serif'] = 'Arial'
+mpl.rcParams['font.stretch'] = 1
+mpl.rcParams['font.weight'] = 'medium'
+mpl.rcParams['axes.labelweight'] = 'bold'
 
-max_elev = 0.109728 #meters
-diff = 0.225
+mg = RasterModelGrid(355,47,0.225) #produces an 80m x 10.67m
+elev = np.zeros(mg.number_of_nodes, dtype = float) #initialize the 
+z = mg.add_field('topographic__elevation', elev, at = 'node')
 
-for i in range(0,30):
-    elev[i:16685:47] = max_elev
-    elev[47-i:16685:47] = max_elev
-    max_elev += diff
+road_peak = 16 #peak crowning height occurs at this point
+up = 0.0067 #slope from ditchline to crown
+down = 0.0035 #slope from crown to hillslope
+
+for i in range(0,355):
+    init_elev = 0
     
-imshow_grid(grid, elev)
+    for j in range(0, 47):
+        z[i*47 + j] = init_elev
+        
+        if j < road_peak:
+            init_elev += up
+        else:
+            init_elev -= down
+
+#z = z + mg.node_y*0.001
+    
+plt.figure(figsize = (4,10))
+imshow_grid(mg, z, var_name = 'Elevation', 
+            var_units = 'm',grid_units = ('m','m'), cmap = 'gist_earth'
+            )
+plt.title('Road Surface Elevation', fontweight = 'bold')
+plt.savefig('C://Users/Amanda/Desktop/RoadSurface.png', bbox_inches = 'tight')
+plt.show()
+
+# =============================================================================
+# Original code from Sai
+# =============================================================================
+
+#from landlab import RasterModelGrid
+#import numpy as np
+#from landlab.io import read_esri_ascii
+#from landlab.plot.imshow import imshow_field, imshow_grid
+
+#grid = RasterModelGrid(53,67,10.)
+#elev = np.zeros(grid.number_of_nodes, dtype = float)
+#
+### For reference - find factors of a number
+##def factors(n):
+##    return set(reduce(list.__add__, ([i, n//i] for i in range(1, \
+#                #int(n**0.5) + 1) if n % i == 0)))
+#
+#max_elev = 1600.
+#diff = 10.
+#for i in range(0,30):
+#    elev[i:3551:67] = max_elev
+#    elev[67-i:3551:67] = max_elev
+#    max_elev -= diff
+#
+#for i in range(30,38):
+#    elev[i:3551:67] = max_elev
