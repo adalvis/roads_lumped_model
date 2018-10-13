be4fd3f43437f4240f26afb4e2d9abcfbff9135f
Amanda Manaster <amanaste@uw.edu>
2018-10-12 17:41:48 -0700

    Update ExnerSolver.py

--- ExnerSolver.py
+++ ExnerSolver.py
@@ -8,27 +8,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
-from landlab.components import FlowRouter, DepressionFinderAndRouter
+from landlab.components import FlowAccumulator, FlowDirectorD8
 from landlab.plot.imshow import RasterModelGrid
 from landlab.plot.imshow import imshow_grid
+from landlab.plot.drainage_plot import drainage_plot
 
 mpl.rcParams['font.sans-serif'] = 'Arial'
-mpl.rcParams['font.stretch'] = 1
+mpl.rcParams['font.stretch'] = 'condensed'
+mpl.rcParams['xtick.top'] = True
+mpl.rcParams['xtick.bottom'] = True
+mpl.rcParams['ytick.right'] = True
+mpl.rcParams['ytick.left'] = True
+mpl.rcParams['xtick.direction'] = 'in'
+mpl.rcParams['ytick.direction'] = 'in'
 
-#%%
 
-init = np.ones([10000])
+#%% Create grid and plot map
+
+init = np.ones([100])
 surface = (init*12) + np.random.rand(init.size)/100000.
 
 
-mg = RasterModelGrid(100, 100, spacing=(1,1))
+mg = RasterModelGrid(10, 10, spacing=(1,1))
 z = mg.add_field('topographic__elevation', surface + mg.node_y*0.05 + mg.node_x*0.05, at = 'node')
 
-mg.set_fixed_value_boundaries_at_grid_edges(True, True, True, True)
-mg.set_closed_boundaries_at_grid_edges(False, False, False, False) 
+#mg.set_fixed_value_boundaries_at_grid_edges(True, True, True, True)
+mg.set_closed_boundaries_at_grid_edges(True, True, True, True) 
 
 plt.figure()
-imshow_grid(mg,z)
+ax = plt.gca()
+ax.tick_params(axis='both', which='both', direction = 'out', bottom = 'on', 
+               left = 'on', top = 'off', right = 'off')
+imshow_grid(mg,z, plot_name = 'Topographic Map of Synthetic Grid', var_name = 'Elevation', 
+            var_units = 'm', grid_units = ('m','m'), cmap = 'jet')
+
+
+#%% Exner Solver
 
 def ExnerSolver(z, dqs_dx):
     
@@ -57,30 +72,9 @@ def qs():
     return (qs)    
 
 
-#%% Pay attention to ####
-def get_ordered_cells(grid, outlet_id=None):
-    """
-    Written by: Sai & Erkan
+#%% This is used to get ordered nodes for the sediment transport
+def ordered(grid, outlet_id=None):
     
-    Runs Landlab's FlowRouter and DepressionFinderAndRouter to
-    route flow. Also orders the cells in the descending order of
-    channel length (upstream cell order).
-    
-    Parameters:
-    ==========    
-    grid: grid object
-        RasterModelGrid
-    outlet_id: int (Optional)
-        Outlet id to be set
-
-    Returns:
-    =======
-    ordered_cells: np.array(dtype=int)
-        cells ordered in descending order of channel length
-    grid: grid object
-        updated RasterModelGrid
-    """
-
     if outlet_id == None:
         outlet_id = np.argmin(grid.at_node['topographic__elevation'])
    
@@ -88,53 +82,15 @@ def get_ordered_cells(grid, outlet_id=None):
                                                     grid.at_node['topographic__elevation'], 
                                                     nodata_value=-9999.)
 #### Change from FlowRouter to FlowDirector and FlowAccumulator
-    flw_r = FlowRouter(grid)
-    flw_r.run_one_step()
-    df = DepressionFinderAndRouter(grid)
-    df.map_depressions()
-    r = grid.at_node['flow__receiver_node'][grid.node_at_core_cell]
-    R = np.zeros(grid.number_of_nodes, dtype=int)
-    R[grid.node_at_core_cell] = r
-    channel_length = np.zeros(grid.number_of_nodes, dtype=int)
+    fa = FlowAccumulator(grid, 
+                         flow_director = FlowDirectorD8)
+    fa.run_one_step()
+    (da, q) = fa.accumulate_flow()
+    ordered_nodes = grid.at_node['flow__upstream_node_order']
     
-    # Compute channel lengths for each node in the wtrshd (node_at_core_cell)
-    for node in grid.node_at_core_cell:
-        node_c = node.copy()
-        while R[node_c] != node_c:
-            channel_length[node] += 1
-            node_c = R[node_c]
-    grid.at_node['channel_length'] = channel_length
-    
-    # Sorting nodes in the ascending order of channel length
-    # NOTE: length of ordered_nodes = grid.number_of_core_cells
-    ordered_nodes = grid.node_at_core_cell[
-        np.argsort(channel_length[grid.node_at_core_cell])]
-    
-    # Sorting nodes in the descending order of channel length
-    ordered_nodes = ordered_nodes[::-1]
-    dd = 1    # switch 2 for while loop
-    count_loops = 0 # No. of loops while runs
-    while dd:
-        dd = 0
-        count_loops += 1
-        sorted_order = list(ordered_nodes)
-        alr_counted_ = []
-        for node_ in sorted_order:
-            donors = []
-            donors = list(grid.node_at_core_cell[np.where(r==node_)[0]])
-            if len(donors) != 0:
-                for k in range(0, len(donors)):
-                    if donors[k] not in alr_counted_:
-                        sorted_order.insert(donors[k], sorted_order.pop(sorted_order.index(node_)))
-                        dd = 1    
-            alr_counted_.append(node_)
-        ordered_nodes = np.array(sorted_order)
-    ordered_cells = grid.cell_at_node[ordered_nodes]
-    
-    return ordered_cells, ordered_nodes, grid
+    return ordered_nodes, grid, da, q
 
 #%% Calculate the slopes for slope-area plots
-
 
 def calculate_slope(grid,z):
     dzdx = grid.calc_grad_at_link(z)
@@ -143,16 +99,25 @@ def calculate_slope(grid,z):
     
     return dzdx
 
+#%%
+ordered_nodes, mg, da, q = ordered(mg)
+plt.figure()
+drainage_plot(mg, 'drainage_area')
+
+
 
 #%%
-ordered_cells, ordered_nodes, mg = get_ordered_cells(mg)
+
 dzdx = calculate_slope(mg, z)
 
 plt.figure()
+plt.title('Slope-Area Plot for Initial Conditions')
+plt.xlabel('Area (m$^2$)')
+plt.ylabel('Slope (-)')
 plt.loglog(mg.at_node['drainage_area'], dzdx, 'ko')
 plt.xlim(10**(0)-10, 10**(4) + 5000)   
 plt.ylim(10**(-4), 10**(-1)) 
-
+plt.show()
 
 #%% Analytical sol'n for k_t = 0.01
 
@@ -173,7 +138,14 @@ def analytical_soln(grid):
 
 slope = analytical_soln(mg)
 drainage_area = mg.at_node['drainage_area'][mg.core_nodes]
+
+plt.figure()
 plt.loglog(drainage_area, slope, 'b-')
+plt.title('Slope-Area Plot of Analytical Solution')
+plt.xlabel('Area (m$^2$)')
+plt.ylabel('Slope (-)')
+plt.xlim(10**(0)-10, 10**(4) + 5000)   
+plt.ylim(10**(-4), 10**(-1)) 
 plt.show()
 
 
