b74a50ae89ac60622951ae6597d12a93b2dbfbd4
Amanda Manaster <amanaste@uw.edu>
2018-10-23 17:53:27 -0700

    Simplified functions by using FlowAccumulator as the only component called.

--- Sediment Transport Model/Functions.py
+++ Sediment Transport Model/Functions.py
@@ -25,37 +25,35 @@ df = DepressionFinderAndRouter(mg)
 
 # ordered() calculates the drainage area for each node and also 
 #   returns the upstream to downstream node order
-def ordered(grid, fa, df, outlet_id=None):
+def ordered(grid, fa, outlet_id=None):
     
-    if outlet_id == None:
-        outlet_id = np.argmin(grid.at_node['topographic__elevation'])
-        grid.set_watershed_boundary_condition_outlet_id(outlet_id, grid.at_node['topographic__elevation'], 
-                                                        nodata_value=-9999.) 
-
+#    if outlet_id == None:
+#        outlet_id = np.argmin(grid.at_node['topographic__elevation'])
+#        grid.set_watershed_boundary_condition_outlet_id(outlet_id, grid.at_node['topographic__elevation'], 
+#                                                        nodata_value=-9999.) 
+    
     fa.run_one_step()
-    df.map_depressions()
-    flooded = np.where(df.flood_status == 3)[0]
     
     da = grid.at_node['drainage_area']
     
     ordered_nodes = grid.at_node['flow__upstream_node_order']
-    ordered_nodes = ordered_nodes[::-1]
+    ordered_nodes = np.flipud(ordered_nodes)
     
-    return (ordered_nodes, grid, da, flooded)
+    return (ordered_nodes, grid, da)
 
 # calculate_slope() determines the slope at each of the links and
 #   maps the maximum slope of the inlinks to the node
 def calculate_slope(grid, z):
     dzdx = grid.calc_grad_at_link(z)
-    grid.at_node['gradient'] = grid.map_downwind_node_link_max_to_node(dzdx)
-    dzdx = grid.at_node['gradient']
+    dzdx = grid.map_downwind_node_link_max_to_node(dzdx)
     
-    return (dzdx, grid)
+    return (dzdx)
 
 
 # sed_disch() calculates sediment discharge used in finding 
 #   the change in elevation of the surface
-def sed_disch(qs, grid, z, ordered_nodes, drainage_area, dzdx, flooded, k_t=0.01, m=2, n=2):
+def sed_disch(qs, qs_in, grid, z, ordered_nodes, drainage_area, dzdx, dzdt, 
+              k_t=0.01, m=2, n=2):
     # $Q_s = k_t A^m S^n$
     # where Q_s = sediment discharge
     #       k_t = erodibility coefficient (choose arbitrarily)
@@ -63,19 +61,16 @@ def sed_disch(qs, grid, z, ordered_nodes, drainage_area, dzdx, flooded, k_t=0.01
     #       S = slope
     #       m & n are empirical constants
     
+    flow_receiver = grid.at_node['flow__receiver_node']
+    
     for node in ordered_nodes:
-        qs[node] = (k_t * (drainage_area[node]**m) * (dzdx[node]**n))
+        qs[node] = qs_in[node] + (k_t * (drainage_area[node]**m) * (dzdx[node]**n))
+        
+        qs_in[flow_receiver[node]] += qs[node]
+        qs[node] = qs_in[node] - dzdt[node] * grid.cell_area_at_node[node]
+        
     
-    
-    if flooded is not None:
-        qs[flooded] = 0.
-    else:
-        reversed_flow = z < z[grid.at_node['flow__receiver_node']]
-        qs[reversed_flow] = 0.
-    
-    grid.at_node['sediment__discharge'] = qs
-    
-    return (qs, grid)   
+    return (qs, qs_in, grid)   
 
 # div_qs() calculates the divergence of sediment discharge at
 #   each node
@@ -92,19 +87,16 @@ def ExnerSolver(grid, z, dzdt, ordered_nodes, dqs_dx, dt):
     
     for node in ordered_nodes:
         dzdt[node] = -dqs_dx[node]
-        z[node] = z[node]-dqs_dx[node]*dt
+        z[node] = z[node] - dqs_dx[node]*dt
             
     return(z, dzdt)
 
-# Q_out() updates the sediment discharge at every node
-def Q_out(qs, dzdt, ordered_nodes, grid):
-    
-    for node in ordered_nodes:
-        if node == 0:
-            qs[node] = -dzdt[node] * grid.cell_area_at_node[node]
-        else:
-            qs[node] = qs[node-1] -dzdt[node] * grid.cell_area_at_node[node]
-        
-    return(qs)   
+## Q_out() updates the sediment discharge at every node
+#def Q_out(qs, qs_in, dzdt, ordered_nodes, grid):
+#    
+#    for node in ordered_nodes:
+#        qs[node] = qs_in[node] - dzdt[node] * grid.cell_area_at_node[node]
+#        
+#    return(qs)   
     
     
