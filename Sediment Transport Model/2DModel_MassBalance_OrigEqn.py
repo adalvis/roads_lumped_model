71d9a83ae74b037199d244acf191f8c208f6e525
Amanda Manaster <amanaste@uw.edu>
2019-02-20 14:57:08 -0800

    make 2d plane model work!

--- Sediment Transport Model/2DModel_MassBalance_OrigEqn.py
+++ Sediment Transport Model/2DModel_MassBalance_OrigEqn.py
@@ -13,7 +13,7 @@ from mpl_toolkits import mplot3d
 
 from landlab.plot.imshow import RasterModelGrid
 from landlab.plot.imshow import imshow_grid
-from landlab.components import FlowAccumulator
+from landlab.components import FlowAccumulator, LinearDiffuser
 from landlab.plot.drainage_plot import drainage_plot
 
 mpl.rcParams['font.sans-serif'] = 'Arial'
@@ -32,14 +32,13 @@ start_time = datetime.now()
 
 #%% Create grid and plot map
 init = np.ones([625])
-surface = (init*12) + np.random.rand(init.size)/100000.
+np.random.seed(2)
+surface = init + np.random.rand(init.size)/55.
 
 mg = RasterModelGrid((25,25), 1)
-z = mg.add_field('topographic__elevation', surface + mg.node_y*0.05 + mg.node_x*0.05, at = 'node')
+z = mg.add_field('topographic__elevation', surface + mg.node_y*0.05, at = 'node')
 
-#outlet_id = np.argmin(mg.at_node['topographic__elevation'])
-#mg.set_watershed_boundary_condition_outlet_id(outlet_id, mg.at_node['topographic__elevation'], 
-#                                   nodata_value=-9999.)
+mg.set_closed_boundaries_at_grid_edges(True, True, True, False)
 
 qs_in = np.zeros(mg.number_of_nodes)
 qs_out = np.zeros(mg.number_of_nodes)
@@ -48,10 +47,10 @@ dzdx = np.zeros(mg.number_of_nodes)
 
 dzdt = np.zeros(mg.number_of_nodes)
 dt = 0.1
-T = np.array([0, 50, 100, 500])
+T = np.arange(0, 1100, 50)
 
 
-plt.figure()
+plt.figure(1)
 ax = plt.gca()
 ax.tick_params(axis='both', which='both', direction = 'out', bottom = True, 
                left = True, top = False, right = False)
@@ -62,6 +61,9 @@ fa = FlowAccumulator(mg, surface = 'topographic__elevation', flow_director = 'Fl
                      depression_finder = 'DepressionFinderAndRouter', routing = 'D8')
 
 fa.run_one_step()
+da = mg.at_node['drainage_area']
+
+ld = LinearDiffuser(mg, linear_diffusivity=0.005)
 
 #%% Calculate slope at each node - initial, analytical
 n = 2.
@@ -80,11 +82,14 @@ drainage_area = drainage_area.copy()
 
 #%% Calculate sediment discharge, divergence, and elevation change     
 t = 0
+
 for l in range(len(T)):
     while t < T[l]:
-
+        
         fa.run_one_step()
-        flooded_nodes = np.where(fa.depression_finder.flood_status == 3)[0]
+#        flooded_nodes = np.where(fa.depression_finder.flood_status == 3)[0]
+        
+        ld.run_one_step(dt)
         
         da = mg.at_node['drainage_area']
         
@@ -107,17 +112,19 @@ for l in range(len(T)):
             else:
                 dqs_dx[src_id] = (qs_out[src_id] - qs_in[src_id]) / mg.cell_area_at_node[src_id]
             
-            if flooded_nodes is not None:
-                qs_out[flooded_nodes] = 0.
-            else:
-                reversed_flow = z < z[dst_id]
-                # this check necessary if flow has been routed across depressions
-                qs_out[reversed_flow] = 0.
-                 
-            dzdt[src_id] = -dqs_dx[src_id]        
+#            if flooded_nodes is not None:
+#                dqs_dx[flooded_nodes] = 0.
+#            else:
+#                reversed_flow = z < z[dst_id]
+#                dqs_dx[reversed_flow] = 0.
+                
+            dzdt[src_id] = -dqs_dx[src_id]    
 
             qs_in[dst_id] = qs_out[src_id]
-        
+            
+            if z[src_id] < z[dst_id]:
+                z[src_id] = z[dst_id]*1.00001
+            
         z0 = z.copy()
         z = z0 + dzdt*dt
             
@@ -125,36 +132,32 @@ for l in range(len(T)):
         t += dt
         print(t)
 
-#%%
-plt.figure()
-drainage_plot(mg, 'drainage_area')
+#    plt.figure()
+#    drainage_plot(mg, 'drainage_area')
+#
+#    plt.figure()
+#    imshow_grid(mg, z, plot_name = '%i years' % T[l], var_name = 'Elevation', 
+#                var_units = 'm', grid_units = ('m','m'), cmap = 'gist_earth', vmin = 1.0, vmax = 2.2)
 
-plt.figure()
-imshow_grid(mg, dzdx, plot_name = 'Slope Plot', var_name = 'Slope', 
-           var_units = 'm', grid_units = ('m','m'), cmap = 'jet')
+    plt.figure()
+    ax2 = plt.axes(projection = '3d')
+    X = mg.node_x.reshape(mg.shape)
+    Y = mg.node_y.reshape(mg.shape)
+    Z = z.reshape(mg.shape)
+    plt.title('%i years' % T[l])
+    ax2.set_xlabel('X (m)', fontsize = 10)
+    ax2.set_ylabel('Y (m)', fontsize = 10)
+    ax2.set_zlabel('Elevation (m)', fontsize = 10)
+    ax2.plot_surface(X, Y, Z, cmap = 'gist_earth')
+#    plt.savefig('C:/Users/Amanda/Desktop/3DFigs/year%i.png' % T[l])
 
-plt.figure()
-imshow_grid(mg, z, plot_name = 'Topographic Map of Synthetic Grid', var_name = 'Elevation', 
-           var_units = 'm', grid_units = ('m','m'), cmap = 'jet')
-
-plt.figure()
-ax2 = plt.axes(projection = '3d')
-X = mg.node_x.reshape(mg.shape)
-Y = mg.node_y.reshape(mg.shape)
-Z = z.reshape(mg.shape)
-plt.title('300 years of evolution')
-ax2.set_xlabel('X (m)', fontsize = 10)
-ax2.set_ylabel('Y (m)', fontsize = 10)
-ax2.set_zlabel('Elevation (m)', fontsize = 10)
-ax2.plot_surface(X, Y, Z, cmap = 'jet')
-
-plt.figure()
-plt.title('Slope-Area Plot for Initial Conditions')
-plt.xlabel('Area (m$^2$)')
-plt.ylabel('Slope (-)')
-plt.loglog(drainage_area, slope[mg.core_nodes], 'b-')
-plt.loglog(da[mg.core_nodes], dzdx[mg.core_nodes], 'ko')
-plt.show()
+    plt.figure()
+    plt.title('Slope-Area Plot for Initial Conditions')
+    plt.xlabel('Area (m$^2$)')
+    plt.ylabel('Slope (-)')
+    plt.loglog(drainage_area, slope[mg.core_nodes], 'b-')
+    plt.loglog(da[mg.core_nodes], dzdx[mg.core_nodes], 'ko')
+    plt.show()
 
 
 #for keeping track of how long the model runs
