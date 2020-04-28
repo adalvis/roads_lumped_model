039b0873535a6fdc939846f00e7548de03edfa86
Amanda Manaster <amanaste@uw.edu>
2020-04-27 18:00:27 -0700

    Fixed up/finalized plots.

--- rlm_model_runs/nehalemRainfall_run.py
+++ rlm_model_runs/nehalemRainfall_run.py
@@ -61,8 +61,8 @@ storms_df.set_index(pd.DatetimeIndex([day0+datetime.timedelta(hours=time)
 #         value from https://pubs.usgs.gov/sir/2008/5093/table7.html
 # n_f = 0.03 #0.0475*(d50)**(1/6); Manning's n of fines in TAF
 L, rho_w, rho_s, g, S, tau_c, d50, d95, n_f = [4.57, 1000, 2650, 
-                                               9.81, 0.0825, 0.04,
-                                               6.25e-5, 0.0375, 0.03]
+                                               9.81, 0.0825, 0.1,
+                                               6.25e-5, 0.0275, 0.03]
 #Define layer constants
 # h_s = depth of surfacing
 # f_sf, f_sc = fractions of fine/coarse material in ballast
@@ -79,7 +79,9 @@ h_b, f_bf, f_br = [2, 0.20, 0.80]
 #   6 tires * 0.225 m width * 0.005 m length * 3.175e-3 m treads
 # u_pb = pumping constant for ballast, m/truck pass
 # e = fraction of coarse material, -
-k_as, k_ab, u_ps, u_pb, e = [1.37e-6, 1.00e-6, 4.69e-6, 2.345e-6, 0.725]
+k_as, k_ab, u_ps, u_pb, e = [1.37e-6, 1.00e-6, 
+                             4.69e-6, 2.345e-6, 
+                             0.725]
 
 data_ser = data_df.groupby(['stormNo', 'intensity'])['tips'].count() # len=426
 int_tip_df = data_ser.reset_index(level=1)
@@ -100,21 +102,23 @@ S_b = np.zeros(len(storms_df))
 S_bc = np.zeros(len(storms_df))
 S_bf = np.zeros(len(storms_df))
 Hs_out = np.zeros(len(storms_df))
-q_s = np.zeros(len(int_tip_df))
-q_s_avg = np.zeros(len(int_tip_df))
 k_s = np.zeros(len(storms_df))
-water_depth = np.zeros(len(int_tip_df))
-rainfall = np.zeros(len(int_tip_df))
-tau = np.zeros(len(int_tip_df))
-tau_e = np.zeros(len(int_tip_df))
 f_s = np.zeros(len(storms_df))
 n_c = np.zeros(len(storms_df))
 n_t = np.zeros(len(storms_df))
 S_f_init = np.zeros(len(storms_df))
 test = np.zeros(len(storms_df))
 
+q_s = np.zeros(len(int_tip_df))
+q_s_avg = np.zeros(len(int_tip_df))
+water_depth = np.zeros(len(int_tip_df))
+rainfall = np.zeros(len(int_tip_df))
+tau = np.zeros(len(int_tip_df))
+tau_e = np.zeros(len(int_tip_df))
+
 n_tp = storms_df.truck_pass.to_numpy()
 t = storms_df.deltaT.to_numpy()
+
 rainfall = int_tip_df.intensity.to_numpy()
 frac = int_tip_df.frac.to_numpy()
 t_storm = int_tip_df.storm_dur.to_numpy()
@@ -158,7 +162,7 @@ for j, storm in enumerate(storms_df.stormNo):
         S_b[j] = S_bc[j] + S_bf[j]
 
     if d95 >= S_f[j-1]:
-        #Let's try using an Sf_init variable because when sediment is added to
+        # Let's try using an Sf_init variable because when sediment is added to
         # TAF, that doesn't change the final S_f value, but it does affect how
         # the sediment is transported!
         sed_added[j] = (q_f1[j]*(t[j]*3600.))/(1-e)
@@ -169,7 +173,7 @@ for j, storm in enumerate(storms_df.stormNo):
     
     if d95 > S_f_init[j]:
         k_s[j] = d95 - S_f_init[j]
-        n_c[j] = 0.075*(k_s[j])**(1/6)
+        n_c[j] = 0.0475*(k_s[j])**(1/6)
     else:
         n_c[j] = 0
     
@@ -198,8 +202,7 @@ for j, storm in enumerate(storms_df.stormNo):
 
     sed_cap[j] = sed_cap[j]*t_storm[k]*3600
 
-
-    # #Create a condition column based on sediment transport capacity vs sediment supply
+    #Create a condition column based on sediment transport capacity vs sediment supply
     value[j] = (sed_added[j]-sed_cap[j])
         
     if value[j] < 0:
@@ -212,135 +215,138 @@ for j, storm in enumerate(storms_df.stormNo):
 
 #Step 2!
 #Add all numpy arrays to the Pandas dataframe
-storms_df['n_t'] = n_t
 storms_df['ks'] = k_s
-storms_df['S_f_init'] = S_f_init
 storms_df['f_s'] = f_s
 storms_df['qf1'] = q_f1
 storms_df['qf2'] = q_f2
-storms_df['dS_f'] = dS_f
-storms_df['S_f'] = S_f
+storms_df['S_f'] = S_f*1000
 storms_df['S_s'] = S_s
 storms_df['S_sc'] = S_sc
 storms_df['S_sf'] = S_sf
 storms_df['S_b'] = S_b
 storms_df['S_bc'] = S_bc
 storms_df['S_bf'] = S_bf
-storms_df['Hs_out'] = Hs_out
+storms_df['Hs_out'] = Hs_out*1000
 storms_df['sed_added'] = sed_added
-storms_df['sed_cap'] = sed_cap
+storms_df['sed_cap'] = sed_cap*1000
 storms_df['val'] = value
 
-plt.figure(figsize=(6,4))
-storms_df.f_s.plot()
+int_tip_df['qs'] = q_s
+int_tip_df['qs_avg'] = q_s_avg
+
+int_tip_df.to_csv('./rlm_output/int_tip_df.csv')
+
+#Plot f_s over time
+fig1, ax1 = plt.subplots(figsize=(6,4))
+storms_df.f_s.plot(ax=ax1)
 plt.xlabel('Date')
 plt.ylabel(r'$f_s$')
 plt.tight_layout()
 plt.show()
 
-#Resample to daily data again
-# df_day_sed = storms_df.resample('D').sum().fillna(0)
-# df_day_sed['day'] = np.arange(0, len(df_day_sed), 1)
-
 #Plot sediment transport rates over time
-# fig2, ax2 = plt.subplots(figsize=(7,5))
-# storms_df.plot(y='q_s', ax=ax2, color = 'peru', legend=False)
-# plt.xlabel('Date')
-# plt.ylabel(r'Sediment transport rate $(m/s)$')
-# plt.title('Sediment transport rates', fontweight='bold', fontsize=14)
-# plt.tight_layout()
-# plt.show()
-
-# df4 = storms_df.resample('D').mean().fillna(method='ffill')
-# df4['day'] = np.arange(0, len(df4), 1)
-# df4['S_f_mm'] = df4.S_f*1000
-# df4['sed_cap_mm'] = df4.sed_cap/1e-3
-# df4['Hs_out_mm'] = df4.Hs_out/1e-3
-
-# fig13, ax13 = plt.subplots(figsize=(6,4))
-# df4.sed_cap_mm.plot(color = '#9e80c2', label='Transport capacity')
-# df4.Hs_out_mm.plot(color='#442766', label='Actual transport')
-# plt.xlabel('Date', fontsize=14, fontweight='bold')
-# plt.ylabel(r'Sediment depth $(mm)$', fontsize=14, fontweight='bold')
-# plt.ylim(0,8)
-# fig13.legend(loc="upper right", bbox_to_anchor=(1,1), bbox_transform=ax13.transAxes)
-# plt.tight_layout()
-# plt.show()
-
-fig1, ax1 = plt.subplots(figsize=(9,4.5))
-storms_df.plot(y='S_f', ax=ax1, color = 'mediumseagreen', legend=False)
-plt.xlabel('Date', fontsize=14, fontweight='bold')
-plt.ylabel(r'Fine sediment storage, $S_f$ $(m)$',fontsize=14, fontweight='bold')
-plt.title('Fine sediment storage', fontweight='bold', fontsize=14)
-#plt.ylim(0,1.4)
+fig2, ax2 = plt.subplots(figsize=(7,5))
+int_tip_df.plot(y='qs', ax=ax2, color = 'peru', legend=False)
+plt.xlabel('Date')
+plt.ylabel(r'Sediment transport rate $(m/s)$')
+plt.title('Sediment transport rates', fontweight='bold', fontsize=14)
 plt.tight_layout()
 plt.show()
 
-# df4['sed_cap_mm'] = df4.sed_cap/1e-3
-# fig8, ax8 = plt.subplots(figsize=(7,3))
-# df4.plot(y='S_f_mm', ax=ax8, color = 'mediumseagreen', legend=False, label='Fine storage')
-# df4.plot(y='Hs_out_mm', ax=ax8, color = '#442766', legend=False, label='Actual transport', alpha=0.75)
-# ax8.set_xlabel('Date', fontsize=14, fontweight='bold')
-# ax8.set_ylabel(r'Sediment depth $(mm)$', fontsize=14, fontweight='bold')
-# ax8.set_ylim(0, 1.4)
-# fig8.legend(loc="upper right", bbox_to_anchor=(1,1), bbox_transform=ax8.transAxes)
-# plt.tight_layout()
-# plt.show()
+#Plot sediment transport capacity and actual transport over time
+fig3, ax3 = plt.subplots(figsize=(6,4))
+storms_df.sed_cap.plot(color = '#9e80c2', label='Transport capacity')
+storms_df.Hs_out.plot(color='#442766', label='Actual transport')
+plt.xlabel('Date', fontsize=14, fontweight='bold')
+plt.ylabel(r'Sediment depth $(mm)$', fontsize=14, fontweight='bold')
+fig3.legend(loc="upper right", bbox_to_anchor=(1,1), 
+    bbox_transform=ax3.transAxes)
+plt.tight_layout()
+#plt.show()
 
-fig10, ax10 = plt.subplots(figsize=(10,7))
-storms_df.plot(y='qf1', ax=ax10, color = 'mediumturquoise', legend=False, label=r'$q_{f1}$')
-storms_df.plot(y='qf2', ax=ax10, color = 'mediumvioletred', legend=False, label=r'$q_{f2}$')
-ax10.set_ylabel(r'Sediment flux $(mm/s)$')
-ax10.set_xlabel('Date')
-fig10.legend(loc="upper right", bbox_to_anchor=(1,1), bbox_transform=ax10.transAxes)
-plt.title('Sediment layer fluxes', fontweight='bold', fontsize=14)
+#Plot fine sediment storage over time
+fig4, ax4 = plt.subplots(figsize=(9,4.5))
+storms_df.plot(y='S_f', ax=ax4, color = 'mediumseagreen', legend=False)
+plt.xlabel('Date', fontsize=14, fontweight='bold')
+plt.ylabel(r'Fine sediment storage, $S_f$ $(mm)$',fontsize=14, 
+    fontweight='bold')
+plt.title('Fine sediment storage', fontweight='bold', fontsize=14)
+plt.tight_layout()
 plt.show()
 
-# df4['diff'] = df4.qf1-df4.qf2
-# fig11, ax11 = plt.subplots(figsize=(10,7))
-# df4.plot(y='diff', ax=ax11, color = 'mediumturquoise', legend=False)
-# ax11.set_ylabel(r'Flux difference, $(mm/s)$')
-# ax11.set_xlabel('Date')
-# plt.show()
+#Plot fine sediment storage and actual transport over time
+fig5, ax5 = plt.subplots(figsize=(7,3))
+storms_df.plot(y='S_f', ax=ax5, color = 'mediumseagreen', legend=False, 
+    label='Fine storage')
+storms_df.plot(y='Hs_out', ax=ax5, color = '#442766', legend=False, 
+    label='Actual transport', alpha=0.75)
+ax5.set_xlabel('Date', fontsize=14, fontweight='bold')
+ax5.set_ylabel(r'Sediment depth $(mm)$', fontsize=14, fontweight='bold')
+fig5.legend(loc="upper right", bbox_to_anchor=(1,1), 
+    bbox_transform=ax5.transAxes)
+plt.tight_layout()
+plt.show()
 
-# df5 = storms_df.resample('D').mean().fillna(method='ffill')
-# #df5['hour'] = np.arange(0, len(df5), 1)
+#Plot transportative fluxes between layers
+fig6, ax6 = plt.subplots(figsize=(10,7))
+storms_df.plot(y='qf1', ax=ax6, color = 'mediumturquoise', 
+    legend=False, label=r'$q_{f1}$')
+storms_df.plot(y='qf2', ax=ax6, color = 'mediumvioletred', 
+    legend=False, label=r'$q_{f2}$')
+ax6.set_ylabel(r'Sediment flux $(mm/s)$')
+ax6.set_xlabel('Date')
+fig6.legend(loc="upper right", bbox_to_anchor=(1,1), 
+    bbox_transform=ax6.transAxes)
+plt.title('Sediment layer fluxes', fontweight='bold', fontsize=14)
+#plt.show()
 
-# fig5, ax5 = plt.subplots(figsize=(6,4))
-# df5.plot(y='S_s', ax=ax5, color = '#532287', legend=False, label='Total surfacing')
-# df5.plot(y='S_sc', ax=ax5, color = '#b0077d', legend=False, label='Coarse surfacing')
-# df5.plot(y='S_sf', ax=ax5, color = '#027fcc', legend=False, label='Fine surfacing')
-# ax5.set_ylabel(r'Surfacing storage, $S_f$ $(m)$', fontweight='bold', fontsize=14)
-# ax5.set_ylim(0, 0.25)
-# ax5.set_title('Surfacing storage', fontweight='bold', fontsize=14)
-# fig5.legend(loc="upper right", bbox_to_anchor=(1,1), bbox_transform=ax5.transAxes)
-# plt.xlabel('Date', fontweight='bold', fontsize=14)
-# plt.tight_layout()
-# plt.show()
+#Plot surfacing storage over time
+fig7, ax7 = plt.subplots(figsize=(6,4))
+storms_df.plot(y='S_s', ax=ax7, color = '#532287', legend=False, 
+    label='Total surfacing')
+storms_df.plot(y='S_sc', ax=ax7, color = '#b0077d', legend=False, 
+    label='Coarse surfacing')
+storms_df.plot(y='S_sf', ax=ax7, color = '#027fcc', legend=False, 
+    label='Fine surfacing')
+ax7.set_ylabel(r'Surfacing storage, $S_f$ $(m)$', fontweight='bold', 
+    fontsize=14)
+ax7.set_title('Surfacing storage', fontweight='bold', fontsize=14)
+fig7.legend(loc="upper right", bbox_to_anchor=(1,1), 
+    bbox_transform=ax7.transAxes)
+plt.xlabel('Date', fontweight='bold', fontsize=14)
+plt.tight_layout()
+plt.show()
 
-# fig7, ax7 = plt.subplots(figsize=(6,4))
-# df5.plot(y='S_b', ax=ax7, color = '#12586b', legend=False, label='Total ballast')
-# df5.plot(y='S_bc', ax=ax7, color = '#099c49', legend=False, label='Coarse ballast')
-# df5.plot(y='S_bf', ax=ax7, color = '#2949e6', legend=False, label='Fine ballast')
-# ax7.set_ylim(0, 0.7)
-# plt.xlabel('Date', fontweight='bold', fontsize=14)
-# plt.ylabel(r'Ballast storage, $S_b$ $(m)$', fontweight='bold', fontsize=14)
-# fig7.legend(loc="upper right", bbox_to_anchor=(1,1), bbox_transform=ax7.transAxes)
-# plt.title('Ballast storage', fontweight='bold', fontsize=14)
-# plt.tight_layout()
-# plt.show()
+#Plot ballast storage over time
+fig8, ax8 = plt.subplots(figsize=(6,4))
+storms_df.plot(y='S_b', ax=ax8, color = '#12586b', legend=False, 
+    label='Total ballast')
+storms_df.plot(y='S_bc', ax=ax8, color = '#099c49', legend=False, 
+    label='Coarse ballast')
+storms_df.plot(y='S_bf', ax=ax8, color = '#2949e6', legend=False, 
+    label='Fine ballast')
+plt.xlabel('Date', fontweight='bold', fontsize=14)
+plt.ylabel(r'Ballast storage, $S_b$ $(m)$', fontweight='bold', 
+    fontsize=14)
+fig8.legend(loc="upper right", bbox_to_anchor=(1,1), 
+    bbox_transform=ax8.transAxes)
+plt.title('Ballast storage', fontweight='bold', fontsize=14)
+plt.tight_layout()
+plt.show()
 
-# fig10, ax10 = plt.subplots(3, figsize=(9,7), sharex=True)
-# df4.plot(y='S_f_mm', ax=ax10[0], color = 'mediumseagreen', legend=False, label='TAF elevation')
-# df5.plot(y='S_s', ax=ax10[1], color = '#532287', legend=False, label='Surfacing elevation')
-# df5.plot(y='S_b', ax=ax10[2], color = '#12586b', legend=False, label='Ballast elevation')
-
-# plt.xlabel('Date', fontweight='bold', fontsize=14)
-# ax10[0].set_ylabel(r'$S_f$ $(mm)$', fontweight='bold', fontsize=14)
-# ax10[1].set_ylabel(r'$S_s$ $(m)$', fontweight='bold', fontsize=14)
-# ax10[2].set_ylabel(r'$S_b$ $(m)$', fontweight='bold', fontsize=14)
-# plt.tight_layout()
-# plt.show()
+fig9, ax9 = plt.subplots(3, figsize=(9,7), sharex=True)
+storms_df.plot(y='S_f', ax=ax9[0], color = 'mediumseagreen', 
+    legend=False, label='TAF elevation')
+storms_df.plot(y='S_s', ax=ax9[1], color = '#532287', legend=False, 
+    label='Surfacing elevation')
+storms_df.plot(y='S_b', ax=ax9[2], color = '#12586b', legend=False, 
+    label='Ballast elevation')
+plt.xlabel('Date', fontweight='bold', fontsize=14)
+ax9[0].set_ylabel(r'$S_f$ $(mm)$', fontweight='bold', fontsize=14)
+ax9[1].set_ylabel(r'$S_s$ $(m)$', fontweight='bold', fontsize=14)
+ax9[2].set_ylabel(r'$S_b$ $(m)$', fontweight='bold', fontsize=14)
+plt.tight_layout()
+plt.show()
 
 # #Subset data by water year
 # # yr_1 = storms_df.Hs_out['2018-10-01':'2019-09-30'].sum()
