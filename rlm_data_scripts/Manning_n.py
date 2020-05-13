eb17dbac13802f303195cb78152079978a1630e8
Amanda Manaster <amanaste@uw.edu>
2020-05-13 16:32:36 -0700

    Plotting tau vs. S_f for varying q

--- rlm_data_scripts/Manning_n.py
+++ rlm_data_scripts/Manning_n.py
@@ -7,6 +7,7 @@ Purpose: To show relationship between Manning's n, q, f_s, tau, H
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import cm
+import matplotlib as mpl
 
 S_f = np.arange(0, 0.0625, 0.00001)
 q = np.linspace(7.43e-6, 0.0003855, len(S_f))
@@ -34,17 +35,21 @@ tau = rho*g*H*S
 tau_s = tau*f_s
 
 plt.close('all')
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(8,5))
+ax.tick_params(bottom=True, top=True, left=True, right=True, which='both')
 
+norm = mpl.colors.Normalize(vmin=5e-6,vmax=0.0005)
+cmap = plt.get_cmap('cividis_r', 25)
+sm = cm.ScalarMappable(norm=norm, cmap=cmap)
 
-color = iter(cm.cool(np.linspace(0,1,25)))
+for j, val in enumerate(np.arange(0, 6249, 250)):
+	ax.plot(S_f, tau_s[val,:], c=cmap(j))
 
-for j in np.arange(0, 6249, 250):
-	c = next(color)
-	ax.plot(S_f, tau_s[j,:], c=c)
-
+cbar = fig.colorbar(sm, ax=ax)
+cbar.ax.set_ylabel(r'$q\ [m^2/s]$', fontsize=12)
 ax.set_xlim(0.0, 0.07)
 ax.set_ylim(0.0, 0.8)
-ax.set_xlabel(r'Fine storage, $S_f$ [m]')
-ax.set_ylabel(r'$\tau_s$ [Pa]')
+ax.set_xlabel(r'Fine storage, $S_f$ [m]', fontsize=12)
+ax.set_ylabel(r'$\tau_s$ [Pa]', fontsize=12)
+ax.set_title(r'Grain shear as a function of $S_f$', fontsize=14)
 plt.show()
