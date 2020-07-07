184a826a0f075c722f536c0eaf37ca0e5d57141a
Amanda Manaster <amanaste@uw.edu>
2020-07-07 14:54:23 -0700

    Switch stations for data

--- rlm_data_scripts/pullRainfall_script.py
+++ rlm_data_scripts/pullRainfall_script.py
@@ -9,8 +9,8 @@ mesonetToken = '081d3a79e9304e25820be19e856bb049'
 #Initialize lists to store data
 date_start = str(201811070000)
 date_end = str(201909170000)
-stn_id = 'SRBW1' #North Fork Toutle; Lat: 46.37194, Lon: -122.57778
-#stn_id = 'EKRW1' #Elk Rock; Lat: 46.312222, Lon: -122.385556
+#stn_id = 'SRBW1' #North Fork Toutle; Lat: 46.37194, Lon: -122.57778
+stn_id = 'EKRW1' #Elk Rock; Lat: 46.312222, Lon: -122.385556
 
 
 #Call API
