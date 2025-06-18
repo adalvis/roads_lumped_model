b302e0150abd10212afd8d91ab328b94cf3a7023
Amanda Alvis <amanaste@uw.edu>
2025-06-18 17:08:28 -0600

    Cleaning code (removing some commented lines, streamlining)

--- rlm_model_runs/rlm_daily_timestep.py
+++ rlm_model_runs/rlm_daily_timestep.py
@@ -116,12 +116,12 @@ for j, date in enumerate(dates):
     if j == 0:
         continue
     else:
-        q_ps_5[j] = u_ps*(S_sf_5[j-1]/S_s_5[j-1])*truck_pass_5[j]#/(timeStep_Hr[j]*3600)
-        q_pb_5[j] = u_pb*(S_bf_5[j-1]/S_b_5[j-1])*truck_pass_5[j]#/(timeStep_Hr[j]*3600)
-        q_cs_5[j] = k_cs*(S_sc_5[j-1]/S_s_5[j-1])*truck_pass_5[j]#/(timeStep_Hr[j]*3600)
-        q_cb_5[j] = k_cb*(S_bc_5[j-1]/S_b_5[j-1])*truck_pass_5[j]#/(timeStep_Hr[j]*3600)
-        S_bc_5[j] = S_bc_5[j-1] - q_cb_5[j]#*(timeStep_Hr[j]*3600)
-        S_sc_5[j] = S_sc_5[j-1] - q_cs_5[j]#*(timeStep_Hr[j]*3600)
+        q_ps_5[j] = u_ps*(S_sf_5[j-1]/S_s_5[j-1])*truck_pass_5[j]
+        q_pb_5[j] = u_pb*(S_bf_5[j-1]/S_b_5[j-1])*truck_pass_5[j]
+        q_cs_5[j] = k_cs*(S_sc_5[j-1]/S_s_5[j-1])*truck_pass_5[j]
+        q_cb_5[j] = k_cb*(S_bc_5[j-1]/S_b_5[j-1])*truck_pass_5[j]
+        S_bc_5[j] = S_bc_5[j-1] - q_cb_5[j]
+        S_sc_5[j] = S_sc_5[j-1] - q_cs_5[j]
         S_sf_5[j] = S_sf_5[j-1] + q_cs_5[j] - q_ps_5[j] \
             + q_pb_5[j]
         S_bf_5[j] = S_bf_5[j-1] + q_cb_5[j] - q_pb_5[j]
@@ -129,11 +129,11 @@ for j, date in enumerate(dates):
         S_s_5[j] = S_sc_5[j] + S_sf_5[j]
         S_b_5[j] = S_bc_5[j] + S_bf_5[j]
 
-    if d95 >= S_f_5[j-1]/(1-e):
-        sed_added_5[j] = (q_ps_5[j])#*(timeStep_Hr[j]*3600.))#/(1-e)
-        S_f_5[j] = S_f_5[j-1] + sed_added_5[j]*(1-e)
-    else:
-        sed_added_5[j] = q_ps_5[j]#*(timeStep_Hr[j]*3600.)
+    # if d95 >= S_f_5[j-1]/(1-e):
+    #     sed_added_5[j] = q_ps_5[j]
+    #     S_f_5[j] = S_f_5[j-1] + sed_added_5[j]*(1-e)
+    # else:
+        sed_added_5[j] = q_ps_5[j]
         S_f_5[j] = S_f_5[j-1] + sed_added_5[j]
 #===========================Detemine qs n = 5===========================
     for k, day in enumerate(dates_rep):
@@ -200,12 +200,12 @@ for j, date in enumerate(dates):
     if j == 0:
         continue
     else:
-        q_ps_10[j] = u_ps*(S_sf_10[j-1]/S_s_10[j-1])*truck_pass_10[j]#/(timeStep_Hr[j]*3600)
-        q_pb_10[j] = u_pb*(S_bf_10[j-1]/S_b_10[j-1])*truck_pass_10[j]#/(timeStep_Hr[j]*3600)
-        q_cs_10[j] = k_cs*(S_sc_10[j-1]/S_s_10[j-1])*truck_pass_10[j]#/(timeStep_Hr[j]*3600)
-        q_cb_10[j] = k_cb*(S_bc_10[j-1]/S_b_10[j-1])*truck_pass_10[j]#/(timeStep_Hr[j]*3600)
-        S_bc_10[j] = S_bc_10[j-1] - q_cb_10[j]#*(timeStep_Hr[j]*3600)
-        S_sc_10[j] = S_sc_10[j-1] - q_cs_10[j]#*(timeStep_Hr[j]*3600)
+        q_ps_10[j] = u_ps*(S_sf_10[j-1]/S_s_10[j-1])*truck_pass_10[j]
+        q_pb_10[j] = u_pb*(S_bf_10[j-1]/S_b_10[j-1])*truck_pass_10[j]
+        q_cs_10[j] = k_cs*(S_sc_10[j-1]/S_s_10[j-1])*truck_pass_10[j]
+        q_cb_10[j] = k_cb*(S_bc_10[j-1]/S_b_10[j-1])*truck_pass_10[j]
+        S_bc_10[j] = S_bc_10[j-1] - q_cb_10[j]
+        S_sc_10[j] = S_sc_10[j-1] - q_cs_10[j]
         S_bf_10[j] = S_bf_10[j-1] + q_cb_10[j] - q_pb_10[j]
         S_sf_10[j] = S_sf_10[j-1] + q_cs_10[j] - q_ps_10[j] \
             + q_pb_10[j]\
@@ -214,11 +214,11 @@ for j, date in enumerate(dates):
         S_b_10[j] = S_bc_10[j] + S_bf_10[j]
         
 
-    if d95 >= S_f_10[j-1]/(1-e):
-        sed_added_10[j] = (q_ps_10[j])#*(timeStep_Hr[j]*3600.))#/(1-e)
-        S_f_10[j] = S_f_10[j-1]*(1-e) + sed_added_10[j]
-    else:
-        sed_added_10[j] = q_ps_10[j]#*(timeStep_Hr[j]*3600.)
+    # if d95 >= S_f_10[j-1]/(1-e):
+    #     sed_added_10[j] = q_ps_10[j]
+    #     S_f_10[j] = S_f_10[j-1]*(1-e) + sed_added_10[j]
+    # else:
+        sed_added_10[j] = q_ps_10[j]
         S_f_10[j] = S_f_10[j-1] + sed_added_10[j]
         
     
@@ -287,12 +287,12 @@ for j, date in enumerate(dates):
     if j == 0:
         continue
     else:
-        q_ps_20[j] = u_ps*(S_sf_20[j-1]/S_s_20[j-1])*truck_pass_20[j]#/(timeStep_Hr[j]*3600)
-        q_pb_20[j] = u_pb*(S_bf_20[j-1]/S_b_20[j-1])*truck_pass_20[j]#/(timeStep_Hr[j]*3600)
-        q_cs_20[j] = k_cs*(S_sc_20[j-1]/S_s_20[j-1])*truck_pass_20[j]#/(timeStep_Hr[j]*3600)
-        q_cb_20[j] = k_cb*(S_bc_20[j-1]/S_b_20[j-1])*truck_pass_20[j]#/(timeStep_Hr[j]*3600)
-        S_bc_20[j] = S_bc_20[j-1] - q_cb_20[j]#*(timeStep_Hr[j]*3600)
-        S_sc_20[j] = S_sc_20[j-1] - q_cs_20[j]#*(timeStep_Hr[j]*3600)
+        q_ps_20[j] = u_ps*(S_sf_20[j-1]/S_s_20[j-1])*truck_pass_20[j]
+        q_pb_20[j] = u_pb*(S_bf_20[j-1]/S_b_20[j-1])*truck_pass_20[j]
+        q_cs_20[j] = k_cs*(S_sc_20[j-1]/S_s_20[j-1])*truck_pass_20[j]
+        q_cb_20[j] = k_cb*(S_bc_20[j-1]/S_b_20[j-1])*truck_pass_20[j]
+        S_bc_20[j] = S_bc_20[j-1] - q_cb_20[j]
+        S_sc_20[j] = S_sc_20[j-1] - q_cs_20[j]
         S_bf_20[j] = S_bf_20[j-1] + q_cb_20[j] - q_pb_20[j]
         S_sf_20[j] = S_sf_20[j-1] + q_cs_20[j] - q_ps_20[j] \
             + q_pb_20[j]
@@ -300,11 +300,11 @@ for j, date in enumerate(dates):
         S_s_20[j] = S_sc_20[j] + S_sf_20[j]
         S_b_20[j] = S_bc_20[j] + S_bf_20[j]
 
-    if d95 >= S_f_20[j-1]/(1-e):
-        sed_added_20[j] = (q_ps_20[j])#*(timeStep_Hr[j]*3600.))#/(1-e)
-        S_f_20[j] = S_f_20[j-1]*(1-e) + sed_added_20[j]
-    else:
-        sed_added_20[j] = q_ps_20[j]#*(timeStep_Hr[j]*3600.)
+    # if d95 >= S_f_20[j-1]/(1-e):
+    #     sed_added_20[j] = q_ps_20[j]
+    #     S_f_20[j] = S_f_20[j-1]*(1-e) + sed_added_20[j]
+    # else:
+        sed_added_20[j] = q_ps_20[j]
         S_f_20[j] = S_f_20[j-1] + sed_added_20[j]
 
 #===========================Detemine qs n = 5===========================
@@ -449,7 +449,7 @@ storms_df.plot(y='S_f_5', ax=ax2, color = '#D5573B', label=r'Mean $n_{\Delta t}$
 plt.xlabel('Date')
 plt.ylabel('Fine sediment storage, $S_a$ (mm)')
 ax2.legend(loc="upper right")
-ax2.set_ylim(19,28)
+ax2.set_ylim(18,21)
 for label in ax2.get_xticklabels():
     label.set_horizontalalignment('center')
 plt.xticks(rotation=0)
@@ -458,19 +458,6 @@ plt.tight_layout()
 plt.show()
 
 #%%
-#Plot fine sediment storage and actual transport over time
-# fig3, ax3 = plt.subplots(figsize=(7,3))
-# storms_df.plot(y='S_f', ax=ax3, color = 'mediumseagreen', legend=False, 
-#     label='Fine storage')
-# storms_df.plot(y='Hs_out', ax=ax3, color = '#442766', legend=False, 
-#     label='Actual transport', alpha=0.75)
-# ax3.set_xlabel('Date')
-# ax3.set_ylabel(r'Sediment depth $(mm)$')
-# fig3.legend(loc="upper right", bbox_to_anchor=(1,1), 
-#     bbox_transform=ax3.transAxes)
-# plt.tight_layout()
-# plt.show()
-# #%%
 #Plot surfacing storage over time
 fig4, ax4 = plt.subplots(figsize=(6,4))
 storms_df.plot(y='S_s_20', ax=ax4, color = '#610345', label=r'Mean $n_{\Delta t}$ = 20 tpd')
@@ -488,15 +475,13 @@ storms_df.plot(y='S_sf_10', ax=ax4, color = '#587A81', legend=False,
     label='Fine surfacing, n=10')
 storms_df.plot(y='S_sf_20', ax=ax4, color = '#587A81', legend=False, 
     label='Fine surfacing,  n=20')
+
 ax4.set_ylabel(r'Surfacing storage, $S_s$ $(m)$')
 ax4.set_ylim(0.05,0.2305)
 ax4.tick_params(axis='x', labelrotation=0)
 for label in ax4.get_xticklabels():
     label.set_horizontalalignment('center')
-plt.legend()
-# ax4.set_title('Surfacing storage')
-# fig4.legend(loc="upper right", bbox_to_anchor=(1,1), 
-#     bbox_transform=ax4.transAxes)
+ax4.legend()
 plt.xlabel('Date')
 plt.tight_layout()
 # plt.savefig(r'C:/Users/Amanda/Documents/GitHub/roads_lumped_model/rlm_output/hourly/surfacing/Surf_%s.png' %S_f_init[0], dpi=300)
@@ -509,7 +494,7 @@ storms_df.plot(y='S_b_20', ax=ax5, color = '#610345', label=r'Mean $n_{\Delta t}
 storms_df.plot(y='S_b_10', ax=ax5, color = '#107E7D', label=r'Mean $n_{\Delta t}$ = 10 tpd')
 storms_df.plot(y='S_b_5', ax=ax5, color = '#D5573B', label=r'Mean $n_{\Delta t}$ = 5 tpd')
 
-# ax5.set_ylim(1.99875, 2.00005)
+ax5.set_ylim(1.99875, 2.00005)
 ax5.tick_params(axis='x', labelrotation=0)
 for label in ax5.get_xticklabels():
     label.set_horizontalalignment('center')
@@ -517,12 +502,9 @@ for label in ax5.get_xticklabels():
 #     label='Coarse ballast')
 # storms_df.plot(y='S_bf', ax=ax5, color = '#39918C', legend=False, 
 #     label='Fine ballast')
-plt.legend()
+ax5.legend()
 plt.xlabel('Date')
 plt.ylabel(r'Ballast storage, $S_b$ $(m)$')
-# fig5.legend(loc="upper right", bbox_to_anchor=(1,1), 
-#     bbox_transform=ax5.transAxes)
-# plt.title('Ballast storage')
 plt.tight_layout()
 # plt.savefig(r'C:/Users/Amanda/Documents/GitHub/roads_lumped_model/rlm_output/hourly/ballast/Bal_%s.png' %S_f_init[0], dpi=300)
 plt.show()
@@ -536,25 +518,22 @@ yr_20 = np.zeros(len(years)-1)
 for i, year in enumerate(years):
     if i < len(years)-1:
         yr_5[i] = (storms_df.Hs_out_5[str(year)+'-07-01':str(year+1)+'-06-30'].sum())/1000
-        # print(yr_5[i])
         yr_10[i] = (storms_df.Hs_out_10[str(year)+'-07-01':str(year+1)+'-06-30'].sum())/1000
-        # print(yr_10[i])
         yr_20[i] = (storms_df.Hs_out_20[str(year)+'-07-01':str(year+1)+'-06-30'].sum())/1000
-        # print(yr_20[i])
 
 #Multiply Hs_out
-print('Sediment depth: ',yr_5)
-print('Total sediment depth: ', yr_5*80*4.57*2)
+# print('Sediment depth: ',yr_5)
+# print('Total sediment depth: ', yr_5*80*4.57*2)
 sed_area_5 = yr_5*L
 sed_load_5 = sed_area_5*rho_s
 
-print('Sediment depth: ',yr_10)
-print('Total sediment depth: ', yr_10*80*4.57*2)
+# print('Sediment depth: ',yr_10)
+# print('Total sediment depth: ', yr_10*80*4.57*2)
 sed_area_10 = yr_10*L
 sed_load_10 = sed_area_10*rho_s
 
-print('Sediment depth: ',yr_20)
-print('Total sediment depth: ', yr_20*80*4.57*2)
+# print('Sediment depth: ',yr_20)
+# print('Total sediment depth: ', yr_20*80*4.57*2)
 sed_area_20 = yr_20*L
 sed_load_20 = sed_area_20*rho_s
 
@@ -567,19 +546,19 @@ plt.bar(years[1:len(years)], sed_load_10, width, color = '#107E7D', edgecolor='k
 plt.bar(years[1:len(years)]+width, sed_load_5, width, color = '#D5573B', edgecolor='k', label=r'Mean $n_{\Delta t}$ = 5 tpd')
 plt.xlabel('Water year')
 plt.ylabel(r'Mass per meter of road (kg/m)')
-# plt.title('Yearly sediment load per meter of road')
 plt.legend()
 plt.xticks(range(ticks[0],ticks[len(ticks)-1]+1), ticks, rotation=0)
 plt.tight_layout()
 # plt.savefig(r'/home/adalvis/github/roads_lumped_model/savefigs/Fig5_three.png', dpi=600)
 plt.show()
 
-# sed_sum_m = storms_df.sed_added.sum()-(storms_df.Hs_out.sum()/1000)
-# sed_sum_kg_m = sed_sum_m*rho_s*L
-# f = ((storms_df.S_f[len(storms_df)-1]-storms_df.S_f[0])/1000)*rho_s*L
+#%%
+sed_sum_m_5 = storms_df.sed_added_5.sum()-(storms_df.Hs_out_5.sum()/1000)
+sed_sum_kg_m_5 = sed_sum_m_5*rho_s*L
+f = ((storms_df.S_f_5.iloc[len(storms_df)-1]-storms_df.S_f_5.iloc[0])/1000)*rho_s*L
 
-# if round(f) == round(sed_sum_kg_m):
-#     print('\nThe mass balance is fine.')
-# else:
-#     print('\nThe mass balance is off.')
-# %%
+#%%
+if round(f) == round(sed_sum_kg_m_5):
+    print('\nThe mass balance is fine.')
+else:
+    print('\nThe mass balance is off.')
